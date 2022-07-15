5a32f486adb5bab5dd04509b419ea8a8d3d09e42
Florian Baumann <flo@noqqe.de>
2022-07-15 21:17:17 +0200

    init

---
+++ radsportsalat.go
@@ -0,0 +1,168 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+var teams = []string{
+	"INEOS",
+	"Grenadiers",
+	"Jumbo",
+	"Visma",
+	"Groupama",
+	"FDJ",
+	"UAE",
+	"Emirates",
+	"BORA",
+	"hansgrohe",
+	"Movistar",
+	"Arkéa Samsic",
+	"Bahrain",
+	"Victorious",
+	"DSM",
+	"Astana",
+	"Qazaqstan",
+	"Cofidis",
+	"Intermarché",
+	"Wanty",
+	"Gobert",
+	"Matériaux",
+	"Trek",
+	"Segafredo",
+	"EF Education",
+	"EasyPost",
+	"Israel Premier Tech",
+	"AG2R",
+	"Citroën",
+	"B&B Hotels",
+	"KTM",
+	"Total",
+	"Energies",
+	"BikeExchange",
+	"Jayco",
+	"Alpecin",
+	"Deceuninck",
+	"Quick-Step",
+	"Alpha Vinyl",
+	"Lotto",
+	"Soudal",
+}
+
+var raceFixes = []string{
+	"Tour",
+	"Race",
+	"Classic",
+	"Tour de",
+	"Il",
+	"La",
+	"Cyclassics",
+	"Criterium du",
+	"Grand Prix",
+	"Giro",
+	"Dwars door",
+	"Cicliste de",
+	"-",
+}
+
+// TODO Proper tokenizing
+var raceNames = []string{
+	"Santos Down Under",
+	"Cadel Evant",
+	"Great Ocean Road",
+	"UAE",
+	"Omloop",
+	"Het Nieuwsblad",
+	"Strade Bianche",
+	"Paris",
+	"Nice",
+	"Tirreno",
+	"Adriatico",
+	"Milano",
+	"Sanremo",
+	"Volta",
+	"Ciclista ",
+	"a Catalunya",
+	"Minerva",
+	"Brugge",
+	"De Panne",
+	"E3 Saxo Bank",
+	"Gent",
+	"Wevelgem",
+	"Vlaanderen",
+	"Ronde van",
+	"Itzulia Basque Country",
+	"Amstel Gold Race",
+	"Paris",
+	"Roubaix",
+	"Flèche",
+	"Wallonne",
+	"Liège",
+	"Bastogne",
+	"Liège",
+	"Romandie",
+	"Eschborn",
+	"Frankfurt",
+	"d'Italia",
+	"Dauphiné",
+	"Suisse",
+	"France",
+	"Donostia San Sebastian Klasikoa",
+	"Pologne",
+	"Vuelta ciclista a España",
+	"BEMER",
+	"Bretagne",
+	"Ouest-France",
+	"Benelux",
+	"Québec",
+	"Montréal",
+	"Lombardia",
+	"Guangxi",
+}
+
+// This is a helper to make decisions
+// based on percentage. Stupid but works.
+func rollDice(prob int) bool {
+	d := rand.Intn(100-1) + 1
+
+	if d < prob {
+		return true
+	}
+
+	return false
+
+}
+
+func genTeamName() string {
+	// TODO RANDOMIZE Number of tokens
+	pick1 := teams[rand.Intn(len(teams))]
+	pick2 := teams[rand.Intn(len(teams))]
+
+	// TODO RANDOMIZE Trenner
+	return "Team " + pick1 + " " + pick2
+}
+
+func genRaceName() string {
+	var pre, post string
+	if rollDice(50) {
+		pre = raceFixes[rand.Intn(len(raceFixes))] + " "
+	}
+	name1 := raceNames[rand.Intn(len(raceNames))]
+	name2 := raceNames[rand.Intn(len(raceNames))]
+
+	if rollDice(50) {
+		post = " " + raceFixes[rand.Intn(len(raceFixes))]
+	}
+
+	// TODO RNADOMIZE Trenner
+	return pre + name1 + " " + name2 + post
+}
+
+func main() {
+	rand.Seed(time.Now().UnixNano())
+	print(genTeamName())
+	fmt.Println()
+	print(genRaceName())
+
+}
